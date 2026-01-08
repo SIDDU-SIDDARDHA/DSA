364bdac710f92e4694de65112bf927399fb97903
Venkata Siddardha <siddardhasiddu946@gmail.com>
2026-01-08 23:12:22 +0530

    Add files via upload

---
+++ stock-buy-sell(with-no-limits).py
@@ -0,0 +1,14 @@
+def maxProfit(prices):
+    res = 0
+
+    # Add all positive differences
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            res += prices[i] - prices[i - 1]
+
+    return res
+
+
+# Driver code
+prices = [100, 180, 260, 310, 40, 535, 695]
+print(maxProfit(prices))
