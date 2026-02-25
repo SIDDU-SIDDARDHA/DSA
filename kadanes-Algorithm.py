de79e6b02ae67ef5ed7e73f55618e93013795f7e
Venkata Siddardha <siddardhasiddu946@gmail.com>
2026-02-25 21:44:38 +0530

    Add files via upload

---
+++ kadanes-Algorithm.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSubarraySum(self, arr):
+        res = arr[0]
+        maxEnding = arr[0]
+        
+        for i in range(1, len(arr)):
+            maxEnding = max(maxEnding + arr[i], arr[i])
+            res = max(res, maxEnding)
+        
+        return res
+
+
+# 🔹 Outside the class
+arr = [2, 3, -8, 7, -1, 2, 3]
+
+obj = Solution()   # Create object
+print(obj.maxSubarraySum(arr))
